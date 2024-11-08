5daa6ad039e29e042c61390c3c2fcbca11f783d6
seonkyo-dpsl <seonkyo.jeong@deepsales.com>
2024-11-08 10:12:45 +0900

    misc: user.data.mock.ts 불필요 주석 제거

--- test/data/user.data.mock.ts
+++ test/data/user.data.mock.ts
@@ -4,7 +4,7 @@ export const userMockData: Readonly<User> = {
   id: 1,
   name: 'John Doe',
   email: 'john.doe@example.com',
-  password: 'hashedPassword123', // 암호화된 비밀번호 예시
+  password: 'hashedPassword123',
   createdAt: new Date('2023-01-01T00:00:00.000Z'),
   updatedAt: new Date('2023-01-01T00:00:00.000Z'),
 };
